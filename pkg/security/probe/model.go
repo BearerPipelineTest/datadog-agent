fefcc8617fa57ac9bfbfef30cebb0003bdf8493b
Gui774ume <guillaume.fournier@datadoghq.com>
2022-02-22 15:48:12 +0100

    rebase fixup

--- pkg/security/probe/model.go
+++ pkg/security/probe/model.go
@@ -9,7 +9,6 @@
 package probe
 
 import (
-	"fmt"
 	"path"
 	"strings"
 	"syscall"
@@ -549,9 +548,7 @@ func (ev *Event) ResolveNetworkDeviceIfName(device *model.NetworkDeviceContext) 
 			device.IfName = tcProbe.IfName
 		}
 	}
-	if ev.probe == nil {
-		fmt.Println("NILLY")
-	}
+
 	return device.IfName
 }
 
